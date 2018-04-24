4ccdacc7c9ca57718bb1616b41afd7e494f03025
EChin4 <36395509+EChin4@users.noreply.github.com>
2018-04-24 11:07:57 -0400

    Update landscape.java

--- landscape.java
+++ landscape.java
@@ -1,16 +1,11 @@
-PImage original, swan;
+PImage original, swan, swan2;
 float xSwan, ySwan, swanSpeed;
-boolean flip;     // doesn't work. in progress
+
 void setup() {
  size(800, 600);
  original = loadImage("pondSwan.jpg"); 
- 
- if (flip == false) {
-   swan = loadImage("swan.png");
- } else if (flip == true) {
-   swan = loadImage("swanTest.jpeg");
- }
- 
+ swan = loadImage("swan.png");
+ swan2 = loadImage("swanTest.jpeg"); 
  xSwan = width * 0.4;
  ySwan = height * 0.63; 
  swanSpeed = 2;
@@ -19,20 +14,21 @@ void setup() {
 void draw() {
  
   image(original, 0, 0, width, height);
- 
+   image(swan, xSwan, ySwan, width*0.09, height*0.09);
  
   xSwan -= swanSpeed;
   
   if (xSwan <= 220) {
     swanSpeed = -swanSpeed;
-    flip = false;
+    image(swan, xSwan, ySwan, width*0.09, height*0.09);
   } else if (xSwan >= width-300) {
     swanSpeed = -swanSpeed;
-    flip = true;
+    image(swan2, xSwan, ySwan, width*0.09, height*0.09);
   }
   
-  image(swan, xSwan, ySwan, width*0.09, height*0.09);
+
   println("x: " + mouseX + "  y: " + mouseY);  
 }
 
 // PVector[] clouds --> PVector is the type. Every element in the array will be a PVector
+// gravity acts on the speed and the speed affects the objects 
