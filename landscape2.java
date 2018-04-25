d2f748334e6965513e5242e353b385da1df020ee
EChin4 <36395509+EChin4@users.noreply.github.com>
2018-04-25 17:18:53 -0400

    Update landscape2.java

--- landscape2.java
+++ landscape2.java
@@ -1,24 +1,24 @@
 float xLake, yLake, xShed, yShed, xShedSize, yShedSize, xBush, yBush, xBushSize, yBushSize, xSwan, ySwan, xSwanSize, ySwanSize, xSwanSpeed;
 
 void setup() {
-  size(1100, 600);
+  size(1200, 600);
   xLake = 0;
-  yLake = 400;
+  yLake = 350;
   
   yShedSize = 200;
   xShedSize = 300;
   xShed = 200;
-  yShed = yLake - yShedSize;
+  yShed = yLake - yShedSize/1.1;
   
   xBush = -25;
   yBush = height*0.65;
   xBushSize = 500;
   yBushSize = 200;
   
-  xSwanSize = 130;
-  ySwanSize = 90;
+  xSwanSize = 160;
+  ySwanSize = 100;
   xSwan = width/2;
-  ySwan = yLake + xSwanSize/2;
+  ySwan = yLake + xSwanSize/1.5;
   xSwanSpeed = 1.5;
 }
 
@@ -26,11 +26,12 @@ void draw() {
   xSwan += xSwanSpeed;
   
   // swan boundaries. Need to flip image when switching sides.
-  if (xSwan >= width - 200 || xSwan <= 300) {
+  if (xSwan >= width - 200 || xSwan <= 450) {
     xSwanSpeed = -xSwanSpeed;
   }
   
-  background(#C7E9FF);
+  //background(#C7E9FF);
+  background(#dcefef);
   noStroke();
   
   // lake
@@ -44,6 +45,12 @@ void draw() {
   // sheds
   fill(#CC0066);
   rect(xShed, yShed, xShedSize, yShedSize);
+  fill(#b69b4c);
+  rect(xShed, yShed + yShedSize, xShedSize, yShedSize/7.5);
+  // bid shed door
+  fill(#864000);
+  rect(xShed + xShedSize/3, yShed + yShedSize/2, xShedSize/3, yShedSize/2);
+  // smaller shed
   fill(#BC005E);
   rect(xShed - 100, yShed + 70, 100, yShedSize - 70);
   
@@ -56,18 +63,29 @@ void draw() {
   ellipse(xBush + 50, yBush + 70, 260, 80);
   
   // swan body. looks more like a duck tbh
-  fill(255);
+  fill(#fcfdfc);
   ellipse(xSwan, ySwan, xSwanSize, ySwanSize);
   // swan neck
-  ellipse(xSwan + xSwanSize*0.45, ySwan - ySwanSize/2, 30, 120);
-  // beak
-  fill(#F9F237);
-  ellipse(xSwan + xSwanSize*0.8, ySwan - ySwanSize, 80, 20);
-  // swan FACE
-  fill(255);
-  ellipse(xSwan + xSwanSize*0.55, ySwan - ySwanSize, 55, 55); 
-  // eye
+  ellipse(xSwan + xSwanSize*0.45, ySwan - ySwanSize*0.65, 30, 145);
+  // beak (yellow/orange)
+  //fill(#F9F237);
+  fill(#fac200);
+  ellipse(xSwan + xSwanSize*0.76, ySwan - ySwanSize*1.12, 80, 20);
   fill(0);
-  ellipse(xSwan + xSwanSize*0.6, ySwan - ySwanSize*1.1, 9, 6);
+  // swan FACE
+  fill(#fcfdfc);
+  ellipse(xSwan + xSwanSize*0.6, ySwan - ySwanSize*1.25, 67, 53);
+  // beak (black)
+  fill(#2c2b2e);
+  ellipse(xSwan + xSwanSize*0.79, ySwan - ySwanSize*1.2, xSwanSize*0.1, ySwanSize*0.25);
+  // eye
+  fill(#473850);
+  ellipse(xSwan + xSwanSize*0.65, ySwan - ySwanSize*1.3, xSwanSize*0.06, ySwanSize*0.12);
+  // wing
+  fill(#f6f4f1);
+  ellipse(xSwan - xSwanSize/6, ySwan - ySwanSize/8, xSwanSize*1.01, ySwanSize*0.85);
   
+  // water covering swan belly
+  fill(#78D4CE);
+  rect(xSwan - xSwanSize*0.7, ySwan + ySwanSize/2.8, xSwanSize*1.2, ySwanSize/4);
 }
